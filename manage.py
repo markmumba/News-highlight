d38f91711ef225ea4b149ff22edd2d66abd62ddf
markmumba <markmumba01@gmail.com>
2019-07-28 20:40:06 +0300

    added

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 from app import create_app
+from flask_script import Manager,Server
+
 
 
 # Creating app instance
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MOVIE_API_KEY='ce9ac29c09f84aaaa7f9320c9bb80adc'
+export SECRET_KEY='bobbyshmurda'
+
