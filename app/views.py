21475991e141c78a5f56b46ba80b216406623ab5
markmumba <markmumba01@gmail.com>
2019-07-28 19:12:14 +0300

    changed my files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_news,get_newss
+
 
 
 
@@ -11,6 +12,27 @@ def index():
     '''
     view root page func that returns index page an func
     '''
+    general = get_news('general')
+    business = get_news('business')
+    technology = get_news('technology')
+    health = get_news('health')
+    science = get_news('science')
+    sports = get_news('sports')
+    
+    title = 'The best news Highlits In The World'
+    
+    search_news_source =request.args.get('news_query')
+
+
+    if search_news_source:
+        return redirect(url_for('main.index',news_name = search_news_source))
+    else:
+        return render_template('index.html',title = title,general = general ,business = business, technology = technology,health=health,science=science,sports=sports)
+
+
+
+
+
     return render_template('index.html')
 
 
