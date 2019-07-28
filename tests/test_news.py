61c755f1a90b4455e5b0589795ca114c53c9e81f
markmumba <markmumba01@gmail.com>
2019-07-28 20:31:00 +0300

    added

--- tests/test_news.py
+++ tests/test_news.py
@@ -9,7 +9,7 @@ from app.models import News
 class NewsTest (unittest.TestCase):
 
     def setUp(self):
-        self.new_news= (1234,"Martin Young","Bitcoin Looks Bearish as Analysts Eye A Drop to Mid-$8,000","A big move was expected for bitcoin price and it came yesterday with a swift plunge back into four figures. The move resulted in a $15 billion dump from crypto market capitalization as altcoins blindly bled out in the shadow of their big brother. Bitcoin Does…", "https://bitcoinist.com/bitcoin-looks-bearish-as-analysts-eye-a-drop-to-mid-8000/",
+        self.new_news= (1,"Martin Young","Bitcoin Looks Bearish as Analysts Eye A Drop to Mid-$8,000","A big move was expected for bitcoin price and it came yesterday with a swift plunge back into four figures. The move resulted in a $15 billion dump from crypto market capitalization as altcoins blindly bled out in the shadow of their big brother. Bitcoin Does…", "https://bitcoinist.com/bitcoin-looks-bearish-as-analysts-eye-a-drop-to-mid-8000/",
  "https://bitcoinist.com/wp-content/uploads/2019/07/28-July-1-e1564304495292-1920x1200.jpg","2019-07-28T09:03:16Z", "A big move was expected for bitcoin price and it came yesterday with a swift plunge back into four figures. The move resulted in a $15 billion dump from crypto market capitalization as altcoins blindly bled out in the shadow of their big brother.\r\nBitcoin Doe… [+3116 chars]")
 
     def test_instance(self):
