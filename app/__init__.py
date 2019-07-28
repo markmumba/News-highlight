8d48171da9d71819437761ae5be346de9d7248be
markmumba <markmumba01@gmail.com>
2019-07-28 16:27:39 +0300

    worflow

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ from flask_bootstrap import Bootstrap
  #initialize the application
 app = Flask(__name__,instance_relative_config = True)
 
-
+#Initializing flask Extension 
+bootstrap = Bootstrap(app)
 
 #setting up configurations 
 app.config.from_object(DevConfig)
